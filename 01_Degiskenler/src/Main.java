5be652b0c6a58cf39f19f3b3f65955469f29705e
= <=>
2021-03-06 01:31:38 +0300

    01

---
+++ 01_Degiskenler/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int a=7;
+        int b=9;
+        int toplam=a+b;
+        
+        System.out.print("merhaba");                               // print alt satira gecmez, println gecer
+        System.out.println(a);                                     // ekrana a nin degerini basar
+        System.out.println("a ile b nin toplamlari:" + toplam);    // , yerine + kullaniliyor.Yorum icin " " kullan.
+        System.out.println((a+b)+" y " + "u "+"z "+a+b+b+b+b);     // parantezin onemi...           
+    }
+    
+}
